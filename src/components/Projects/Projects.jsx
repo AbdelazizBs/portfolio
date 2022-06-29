10780b55df2adc5d5f694672629e02b76ab6d19c
Bassem Dimassi <dimassibassem99@gmail.com>
2022-06-30 00:32:40 +0100

    fix a bug

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -25,7 +25,7 @@ function Projects() {
         <BlogCard key={index}>
           <Img src={p.image} />
           <TitleContent>
-            <HeaderThree title>{p.title}</HeaderThree>
+            <HeaderThree title='true'>{p.title}</HeaderThree>
             <Hr />
           </TitleContent>
           <CardInfo className='card-info'>{p.description}</CardInfo>
