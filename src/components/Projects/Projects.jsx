066a43681956ecef2c0c7b11d9b8846ad4228995
Bassem Dimassi <dimassibassem99@gmail.com>
2022-08-05 16:35:52 +0100

    :constructor: Work in progress

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,7 +10,7 @@ import {
   CardInfo,
   ExternalLinks,
   HeaderThree,
-  Hr
+  Hr, TitleContent,
 } from './ProjectsStyles'
 import { SectionTitle } from '../../styles/GlobalComponents'
 import { projects } from '../../constants/constants'
@@ -24,17 +24,18 @@ const Projects = () =>
       effect='cards'
       grabCursor
       modules={[EffectCards]}
-      className='xs:w-[30rem] sm:w-[35rem] md:w-[40rem] lg:w-[50rem] xl:w-[50rem]'
+      className=' xs:w-[40rem] sm:w-[43rem] md:w-[45rem] lg:w-[50rem] xl:w-[50rem]'
     >
+
       {projects.map((p, index) =>
-        <SwiperSlide className='bg-[#212d45]'>
-          <div key={index}>
+
+        <SwiperSlide key={index} className='rounded-xl shadow-md shadow-amber-50 bg-[#212d45]'>
             <img className='w-full h-[20rem] ' src={p.image} alt={p.alt} />
-            <div>
-              <HeaderThree title='true'>{p.title}</HeaderThree>
+            <TitleContent>
+              <HeaderThree title={p.title}>{p.title}</HeaderThree>
               <Hr />
-            </div>
-            <CardInfo className='card-info'>{p.description}</CardInfo>
+            </TitleContent>
+            <CardInfo>{p.description}</CardInfo>
             <div>
               <ul className='flex flex-wrap justify-around p-6 gap-x-3 gap-y-20 my-8'>
                 {p.tags.map((t, i) => i !== 4 ?
@@ -42,10 +43,11 @@ const Projects = () =>
                   <div key={i + i + 1} className='flex-wrap'><ExternalLinks key={i}>{t}</ExternalLinks></div>)}
               </ul>
             </div>
-          </div>
         </SwiperSlide>
       )}
+
     </Swiper>
+
   </div>
 
 export default Projects
--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,18 +1,18 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const GridContainer = styled.section`
-display: grid;
-grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
-padding: 3rem;
-place-items: center;
-column-gap: 2rem;
-row-gap: 3rem;
-@media ${(props) => props.theme.breakpoints.sm} {
-  display: flex;
-  flex-direction: column;
-  padding: 2rem;
-  padding-bottom: 0;
-}
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  padding: 3rem;
+  place-items: center;
+  column-gap: 2rem;
+  row-gap: 3rem;
+  @media ${(props) => props.theme.breakpoints.sm} {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    padding-bottom: 0;
+  }
 
 `
 export const BlogCard = styled.div`
@@ -23,13 +23,13 @@ export const BlogCard = styled.div`
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
-`;
+`
 export const TitleContent = styled.div`
   text-align: center;
   z-index: 20;
   width: 100%;
 
-`;
+`
 
 
 export const HeaderThree = styled.h3`
@@ -38,7 +38,7 @@ export const HeaderThree = styled.h3`
   color: #9cc9e3;
   padding: .5rem 0;
   font-size: ${(props) => props.title ? '3rem' : '2rem'};
-`;
+`
 
 export const Hr = styled.hr`
   width: 50px;
@@ -46,7 +46,7 @@ export const Hr = styled.hr`
   margin: 20px auto;
   border: 0;
   background: #d0bb57;
-`;
+`
 
 export const Intro = styled.div`
   width: 170px;
@@ -56,20 +56,15 @@ export const Intro = styled.div`
   font-size: 13px;
   font-style: italic;
   line-height: 18px;
-`;
+`
 
 
 export const CardInfo = styled.p`
-  width: 100%;
-  padding: 0 50px;
   color: #e4e6e7;
   line-height: 24px;
   text-align: justify;
-  @media ${(props) => props.theme.breakpoints.sm} {
-    padding:.3rem
-  
-}
-`;
+  padding: 3rem;
+`
 
 
 export const UtilityList = styled.ul`
@@ -78,28 +73,29 @@ export const UtilityList = styled.ul`
   display: flex;
   justify-content: space-around;
   margin: 2.5rem 0;
-`;
+`
 
 export const ExternalLinks = styled.a`
   width: 100%;
-color:#d4c0c0;
-font-size: 1.6rem;
-padding:1rem 1.5rem;
-background: #6b3030;
-border-radius: 15px;
-transition: 0.5s;
-&:hover{
-  background: #801414;
+  color: #d4c0c0;
+  font-size: 1.6rem;
+  padding: 1rem 1.5rem;
+  background: #6b3030;
+  border-radius: 15px;
+  transition: 0.5s;
 
-}
-`;
+  &:hover {
+    background: #801414;
+
+  }
+`
 
 export const TagList = styled.ul`
-display: grid;
-justify-content: space-around;
-padding: 2rem;
+  display: grid;
+  justify-content: space-around;
+  padding: 2rem;
 `
 export const Tag = styled.li`
-color: #d8bfbf;
-font-size: 1.5rem;
+  color: #d8bfbf;
+  font-size: 1.5rem;
 `
