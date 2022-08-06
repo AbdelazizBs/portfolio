2a7e670e5c9528144c93d7d7a779e0ccb3b5a66b
Bassem Dimassi <dimassibassem99@gmail.com>
2022-08-06 23:10:13 +0100

    Production image

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,26 +9,25 @@ import { Layout } from '../layout/Layout'
 import { Section } from '../styles/GlobalComponents'
 
 const Home = () => (
-      <Layout>
-        <Head>
-          <title>Portfolio</title>
-          <meta charSet='UTF-8' />
-          <meta property='og:image' content='/images/Portfolio.jpg' />
-          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-          <meta name='description' content='Web developer portfolio' />
-          <meta name='keywords' content='HTML, CSS, JavaScript' />
-          <meta name='author' content='Bassem Dimassi' />
-        </Head>
-        <Section grid>
-          <Hero />
-          <BgAnimation />
-        </Section>
-        <Projects />
-        <Technologies />
-        <TimeLine />
-        <Acomplishments />
-      </Layout>
-
+    <Layout>
+      <Head>
+        <title>Portfolio</title>
+        <meta charSet='UTF-8' />
+        <meta property='og:image' content='/images/PortfolioBassem.jpg' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='description' content='Web developer portfolio' />
+        <meta name='keywords' content='HTML, CSS, JavaScript' />
+        <meta name='author' content='Bassem Dimassi' />
+      </Head>
+      <Section grid>
+        <Hero />
+        <BgAnimation />
+      </Section>
+      <Projects />
+      <Technologies />
+      <TimeLine />
+      <Acomplishments />
+    </Layout>
   )
 
 export default Home
